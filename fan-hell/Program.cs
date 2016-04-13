fd3e34f4f5e5c280d3f788713f10ea66786174b5
PixxoWolf <dfrias41@gmail.com>
2016-04-13 17:47:50 -0500

    heck

---
+++ fan-hell/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace fanhell
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int loop = -1;
+			while (loop < 0) 
+			{
+				Console.WriteLine ("The Ride Never Ends");
+			}
+		}
+	}
+}
